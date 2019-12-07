64c3a9dc64c3877fb12f40a47d1b37d5d9450504
sreeveena <sreeveena21@gmail.com>
2019-12-06 16:16:51 -0800

    word.js

--- letter.js
+++ letter.js
@@ -21,3 +21,4 @@ function Letter(char){
     }
    
 };
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,8 +1,30 @@
+//Contains a constructor, Word that depends on 
+//the Letter constructor. This is used to create 
+//an object representing the current word the user 
+//is attempting to guess
+
+var Letter = require("./letter.js");
 var Word = function(randomWord){
-    this.dash = "_";
     this.randomWord = randomWord;
     this.getGuessedWord = function(){
-        
+        var guessedWord = "";
+        for( var i = 0; i < this.letterArray.length; i++){
+            guessedWord += this.letterArray[i].get();
+        } 
+        return guessedWord;
     }
-   
+    this.prepareWord = function(){
+        var j = 0;
+        for( var i = 0; i < this.randomWord.length; i++){
+            this.letterArray[j] = new Letter(this.randomWord[i]);
+           j++;
+        }
+    }
+    this.checkLetter = function(char){
+        for( var i = 0; i < this.letterArray.length; i++){
+            this.letterArray[i].check(char);
+        } 
+    }
+    
 }
+module.exports = Word;
